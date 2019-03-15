ce78ae392c46475404d64f1675468b72bc55e9c3
Lam, Alice <Alice_Lam@Dell.com>
2019-03-15 15:00:23 -0500

    basic working page for appointments

---
+++ src/main/java/springapp/controller/AppointmentController.java
@@ -0,0 +1,41 @@
+package springapp.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import springapp.command.ClientCommand;
+import springapp.domain.Client;
+import springapp.service.ClientService;
+
+/**
+ * This controller handles all client related pages
+ *
+ * Notice the @PreAuthorize annotations on the methods
+ */
+@Controller
+@RequestMapping("/appointments") // notice that this path is set at the class level.
+public class AppointmentController {
+
+	// Inject in a ClientService claass
+	@Autowired
+	//AppointmentService appointmentService;
+	
+	@PreAuthorize("hasAuthority('LIST_CLIENTS')")
+	@GetMapping
+	 public String listAppointments() {
+  
+       return "appointments/listAppointments";
+   }
+
+}
