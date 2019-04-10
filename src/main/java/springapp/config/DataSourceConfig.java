7de9a6458f465d1151736266e3db2440d939cd84
annie tran <nntrn@me.com>
2019-04-10 15:11:24 -0500

    maybe a fix: jdbc/jpa/hibernate

--- src/main/java/springapp/config/DataSourceConfig.java
+++ src/main/java/springapp/config/DataSourceConfig.java
@@ -4,18 +4,21 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.sqlite.SQLiteDataSource;
- 
+
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
- 
+
 @Configuration
 public class DataSourceConfig {
- 
+
     @Bean(destroyMethod = "", name = "EmbeddeddataSource")
-    public DataSource dataSource() {
+    public DataSource dataSource() throws SQLException {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.sqlite.JDBC");
         dataSourceBuilder.url("jdbc:sqlite:" + "spring-app.db");
         dataSourceBuilder.type(SQLiteDataSource.class);
+        
         return dataSourceBuilder.build();
     }
  
