4d0ae2b1c7644307bede72012c04f15afceb51ae
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2019-04-11 12:10:28 -0500

    fixed resaving lost client name and pet name

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,26 +1,21 @@
 create table IF NOT EXISTS clients (
 	id integer PRIMARY KEY AUTOINCREMENT,
-	name varchar(255) UNIQUE,
+	name varchar(255),
 	phone_number varchar(255),
 	address varchar(255),
-	UNIQUE(name,phone_number)
+	UNIQUE(name, address, phone_number)
 );
 
-ALTER TABLE clients
-ADD CONSTRAINT unique_constraint_name_address_phonenumber UNIQUE(name, address, phone_name);
- 
 create table IF NOT EXISTS pets (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	name varchar(255),
 	gender varchar(255),
 	altered boolean,
 	client_id integer,
-	FOREIGN KEY (client_id) REFERENCES clients(id)
+	FOREIGN KEY (client_id) REFERENCES clients(id),
+	UNIQUE(name, client_id)
 );
 
-ALTER TABLE pets
-ADD CONSTRAINT unique_constraint_petname_clientid UNIQUE(name, client_id);
- 
 create table IF NOT EXISTS users (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	username varchar(255) UNIQUE,
@@ -36,5 +31,6 @@ create table IF NOT EXISTS appointments (
 	appt_date varchar(255),
 	appt_type varchar(255),
 	FOREIGN KEY (client_id) REFERENCES clients(id),
-	FOREIGN KEY (pet_id) REFERENCES pets(id)
+	FOREIGN KEY (pet_id) REFERENCES pets(id),
+	UNIQUE(client_id, pet_id, appt_time, appt_date)
 );
